0927bad37ed141e31e09cdbc3bd1e28d93c3cf29
Carlos <carlosjesus@discente.ufg.br>
2022-11-03 01:48:15 -0300

    Third commit py demand request

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ def print_hi(name):
     v1 = input(f'Hi, {name}, how are you?')
     if v1 == "good":
         print("I'm happy for you!")
+    elif v1 == "i don't now":
+        print("What can i do to improve?")
     else:
         print("Sad, get well!")
 
@@ -11,3 +13,4 @@ if __name__ == '__main__':
 
 # Código comentado para teste do git
 # Estou aprendendo git
+# Até agora está dando certo, vamos para a proxima etapa!
