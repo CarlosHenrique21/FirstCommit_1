5c3769dbda1cc06c734642001201d7b66ad275ef
Carlos <carlosjesus@discente.ufg.br>
2022-11-03 01:32:16 -0300

    First Commit in Py

---
+++ main.py
@@ -0,0 +1,10 @@
+def print_hi(name):
+    v1 = input(f'Hi, {name}, how are you?')
+    if v1 == "good":
+        print("I'm happy for you!")
+    else:
+        print("Sad, get well!")
+
+if __name__ == '__main__':
+    print_hi('Carlos Henrique')
+
