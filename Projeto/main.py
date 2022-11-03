083d597d8e1e57bae8d53d692a19ee0189b82256
Carlos <carlosjesus@discente.ufg.br>
2022-11-03 02:17:57 -0300

    First Commit in py final

---
+++ Projeto/main.py
@@ -0,0 +1,18 @@
+def print_hi(name):
+    v1 = input(f'Hi, {name}, how are you?')
+    if v1 == "good":
+        print("I'm happy for you!")
+    elif v1 == "i don't now":
+        print("What can i do to improve?")
+    else:
+        print("Sad, get well!")
+
+if __name__ == '__main__':
+    print_hi('Carlos Henrique') # Print_hi é o print que tem meu nome
+
+
+# Código comentado para teste do git
+# Estou aprendendo git
+# Até agora está dando certo, vamos para a proxima etapa!
+# Essa etapa aqui é de um clone do meu repositorio do git --- por isso o nome teste_clone
+# Mais um teste - tst
